83d4e84709ad6fef719865e1acb1131d319ba148
vldiaz <victordiaz24@gmail.com>
2021-01-22 15:25:59 -0300

    Se setea el modelo de entidades base, se agrego una entidad fuente de inversion

--- seguimiento/admin.py
+++ seguimiento/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import EntidadPropietaria, TipoAccion, Municipio, Programa, TipoEstado
+from .models import EntidadPropietaria, TipoAccion, Municipio, Programa, TipoEstado, FuenteInversion, Registro
 
 admin.site.register(EntidadPropietaria)
 
@@ -12,3 +12,7 @@ admin.site.register(Municipio)
 admin.site.register(Programa)
 
 admin.site.register(TipoEstado)
+
+admin.site.register(FuenteInversion)
+
+admin.site.register(Registro)
--- seguimiento/models.py
+++ seguimiento/models.py
@@ -35,9 +35,39 @@ class Programa(models.Model):
         return self.nombre
 
 
+class FuenteInversion(models.Model):
+
+    nombre = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nombre
+
+
 class TipoEstado(models.Model):
 
     nombre = models.CharField(max_length=200)
     
     def __str__(self):
         return self.nombre
+
+
+class Registro(models.Model):
+
+    id_tipo_accion = models.ForeignKey(TipoAccion, null=True, blank=True, on_delete=models.CASCADE)
+
+    id_tipo_estado = models.ForeignKey(TipoEstado, null=True, blank=True, on_delete=models.CASCADE)
+
+    id_municipio = models.ForeignKey(Municipio, null=True, blank=True, on_delete=models.CASCADE)
+
+    cantidad = models.IntegerField(default = 0, blank = True)
+
+    id_fuente_inversion =  models.ForeignKey(FuenteInversion, null=True, blank=True, on_delete=models.CASCADE)
+
+    id_programa = models.ForeignKey(Programa, null=True, blank=True, on_delete=models.CASCADE)
+
+    monto = models.CharField(max_length=10)
+
+    anio = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.anio
